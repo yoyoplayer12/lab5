a96391c9b4c8aefc29d054a6393c346604d8339b
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-11-27 19:11:30 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import VideoCard from './components/VideoCard.vue';
+import CommentSection from './components/CommentSection.vue';
+import UsernameSpace from './components/UsernameSpace.vue';
+</script>
+
+<template>
+  <div class="lab">
+    <div class="videocard">
+      <VideoCard />
+    </div>
+    <div class="commentsection">
+      <UsernameSpace />
+      <CommentSection />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.videocard {
+  width: 50vw;
+  height: 100vh;
+}
+
+.commentsection {
+  width: 50vw;
+  height: 100vh;
+  background-color: #f2f2f2;
+}
+
+.lab {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/CommentSection.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    CommentSection
+</template>
+
+<style scoped>
+
+</style>
